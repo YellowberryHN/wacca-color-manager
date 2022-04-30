0b891839ecd791e77679574e24dcca4b7720e291
Zsolt Zitting <yellowberry@cocaine.ninja>
2022-04-29 18:59:34 -0600

    Loading presets might work?

--- main.js
+++ main.js
@@ -10,14 +10,117 @@ export function main(){
 		loadPreset(prompt('プリセットコードを入力して下さい:\nPlease enter a preset code:'));
 	}
 
-	window.loadPreset = function(preset) {
-		console.log("preset code given: " + preset);
-		var b64 = preset.replace(/[\+\/\=]/g, a => ({'+':'.','/':',','=':'$'})[a]); 
-		var raw = Uint8Array.from(atob(b64), c => c.charCodeAt(0));
+	window.loadPreset = function(presetCode) {
+		if(presetCode == null) return false;
+
+		// default colors as of reverse
+		preset = {
+			my: 303001,
+			touchNote: 5,
+			chainNote: 6,
+			slideNoteLeft: 4,
+			slideNoteRight: 3,
+			snapNoteUp: 1,
+			snapNoteDown: 2,
+			holdNote: 7
+		};
+
+		console.log("preset code given: " + presetCode);
+		var b64 = presetCode.replace(/[\!\&\$]/g, a => ({'!':'+','&':'/','$':'='})[a]);
+		var raw = null;
+
+		try {
+			raw = Uint8Array.from(atob(b64), c => c.charCodeAt(0));
+		} catch(e) {
+			if(e instanceof DOMException) {
+				alert("コードが無効です。もう一度やり直してください。\nInvalid code. Please try again. [raw]");
+				return false;
+			}
+			else console.error(e);
+		}
+
+		function va(array, start, length = 2) {
+			var value;
+			for (var i = start; i < start + length; i++) {
+			    value = (value << 8) | array[i];
+			}
+			return value;
+		}
+
+		if(raw != null) {
+			if(raw.length != 17) {
+				alert("コードが無効です。もう一度やり直してください。\nInvalid code. Please try again. [len]");
+				return false;
+			}
+
+			/* hello? jank department?? */
+
+			preset.my = va(raw, 0, 3);
+
+			preset.touchNote = va(raw, 3);
+			preset.chainNote = va(raw, 5);
+			preset.slideNoteLeft = va(raw, 7);
+			preset.slideNoteRight = va(raw, 9);
+			preset.snapNoteUp = va(raw, 11);
+			preset.snapNoteDown = va(raw, 13);
+			preset.holdNote = va(raw, 15);
+
+			for (const prop in preset) {
+				console.log(`${prop}: ${preset[prop]}`);
+
+				fetch(`https://wacca.marv-games.jp/web/option/${prop}Color/exec`, {
+				  "headers": {
+				    "content-type": "application/x-www-form-urlencoded",
+				  },
+				  "referrer": `https://wacca.marv-games.jp/web/option/${prop}Color`,
+				  "body": `optionValue=${preset[prop]}`,
+				  "method": "POST",
+				  "mode": "cors",
+				  "credentials": "include"
+				});
+			}
+
+			return preset;
+		}
+		
 		console.log(raw);
 	}
 
-	window.generatePreset = function() {
+	window.grabColors = function() {
+		preset = {
+			my: 0,
+			touchNote: 0,
+			chainNote: 0,
+			slideNoteLeft: 0,
+			slideNoteRight: 0,
+			snapNoteUp: 0,
+			snapNoteDown: 0,
+			holdNote: 0
+		};
+
+		for (const prop in preset) {
+			
+			fetch(`https://wacca.marv-games.jp/web/option/${prop}Color`, {
+			  "referrer": `https://wacca.marv-games.jp/web/option/designSetting`,
+			  "method": "GET",
+			  "mode": "cors",
+			  "credentials": "include"
+			});
+
+			// fix this, don't be a clown.
+			const selectedColor = document.querySelector('.option_image_select_content.selected #option_value').value;
+
+			console.log("got "+selectedColor);
+
+			preset[prop] = selectedColor;
+
+			console.log(`${prop}: ${preset[prop]}`);
+		}
+
+		return preset;
+	}
+
+	window.generatePreset = function(preset) {
 		alert("not implemented.");
 	}
 
